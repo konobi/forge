bdd5953108aff9fa2d57cbee34e90f6f6c392e00
Scott McWhirter <scott.mcwhirter@joyent.com>
2014-06-04 16:54:27 -0700

    Move everything into it's right place

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-Currently in development... most of the code in development is in
-testground/dhcpd.js to get that knuckled down.
+Currently in development...
+
+This project aims to assist with managing netboot infrastructure from a single
+server process.
+
+This should also allow for custom workflows based on information in a CMDB
+like system, for things like firmware updates, fact gathering, automated
+installs and more.
 
 LICENSE: MIT
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 var forge = require('./lib/forge');
+var mem_store = require('./lib/memory-store');
+
+var store = new mem_store();
 
 var server = new forge({
     subnet: '172.16.184.0/24',
@@ -8,35 +11,16 @@ var server = new forge({
     range_end: '172.16.184.60',
     routers: [ '172.16.184.1' ],
     nameservers: [ '8.8.8.8', '8.8.4.4' ],
-    set_lease: set_lease,
-    save_lease: save_lease,
-    get_lease: get_lease,
-    remove_lease: remove_lease
+    save_lease: function(lease, cb){
+      store.save_lease(lease, cb); },
+    get_lease: function(mac_addr, cb){
+      store.get_lease(mac_addr, cb); },
+    get_lease_by_ip: function(ip, cb){
+      store.get_lease_by_ip(ip, cb); },
+    get_next_ip: function(cb){
+      store.get_next_ip(cb); },
+    remove_lease: function(mac_addr, cb){
+      store.remove_lease(mac_addr, cb); },
+    host: '192.168.119.1'
 });
 
-var leases = {};
-var ip_2_mac = {};
-
-
-function set_lease (offer, pkt) {
-  // We have a new client, so lets make sure we set everything appropriately
-  var lease = leases[pkt.chaddr];
-
-  // the client has supplied the IP, so lets check it's availability
-};
-
-/*
- * XXX - Lets do in memory for now
- *
-initialize_lmdb = function initialize_lmdb (opts) {
-  var self = this;
-  var env = new lmdb.Env();
-  env.open({
-        path: "./data/forge.db"
-  });
-  var dbi = env.openDbi({
-    name: opts.ip_range,
-    create: true
-  });
-};
-*/
